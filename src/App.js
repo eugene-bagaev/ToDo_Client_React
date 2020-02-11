b927abe40a2c842d87431d41a4a630c02e774d8c
Eugene Bagaev <eugene.bagaev@gmail.com>
2020-02-11 23:50:48 +0300

    updated main file

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+// import Contacts from './components/contacts';
 
 // server api for all data http://46.101.211.139:3000/
 
-function MyCmp() {
-  return (
-      <div>Test Text</div>
-  )
-}
+class App extends Component {
+    render() {
+        return (
+            <h1>Hello</h1>
+        )
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. <MyCmp />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Easy
-        </a>
-      </header>
-    </div>
-  );
+    state = {
+        contacts: []
+    };
+
+    componentDidMount() {
+        console.log('MOUNT')
+        fetch('http://46.101.211.139:3000/', {
+            headers: {
+                "Content-Type": "text/plain"
+            },
+        })
+            .then(res => {
+                console.log('DATA: ', res.json());
+                res.json()
+            })
+            .then((data) => {
+                this.setState({ contacts: data })
+            })
+            .catch(console.log)
+    }
 }
 
 export default App;
